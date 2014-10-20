8004f03f8f6ecdfdf432b836d2eab4d492896dc9
Philur <Philur@users.noreply.github.com>
2014-10-20 11:35:41 +0200

    Update TestBankAccount.java

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -85,7 +85,7 @@ public class TestBankAccount {
 	
 	@Test
 	public void testKreditWithNegativeFunds5() {
-		BankAccount account = new BankAccount(100);
+		BankAccount account = new BankAccount(10);
 		double amount = account.kredit(-5.50);
 		Assert.assertEquals(10.0, amount);
 	}
